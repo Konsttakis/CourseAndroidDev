e9f18f7a02a55b538189885db3549ae661c14fd0
Konstantinos <konsttakis@csd.auth.gr>
2024-05-17 12:11:38 +0300

    Added DataBaseHandler. Small Changes in Chapter for it to work with the DB

--- app/src/main/java/com/example/maintabviews/Chapter.java
+++ app/src/main/java/com/example/maintabviews/Chapter.java
@@ -1,14 +1,20 @@
 package com.example.maintabviews;
 
 public class Chapter {
+
+    private String courseName;
     private String name;
     private boolean isCompleted = false;
 
-    public Chapter(String name, boolean isCompleted) {
+    public Chapter(String courseName, String name, boolean isCompleted) {
+        this.courseName = courseName;
         this.name = name;
         this.isCompleted = isCompleted;
     }
 
+    public String getCourseName() {
+        return courseName;
+    }
     public String getName() {
         return name;
     }
